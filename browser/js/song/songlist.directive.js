a211ad78f129f1f4143134d2cb81b6d1e220599c
ishdaddy7 <seanhan1@gmail.com>
2016-07-11 20:42:08 -0400

    finished project (except bonus)

--- browser/js/song/songlist.directive.js
+++ browser/js/song/songlist.directive.js
@@ -28,3 +28,16 @@ juke.directive('songList', function(PlayerFactory) {
 
 	}
 })
+.directive('doubleClick', function(){
+	return {
+		restrict: 'A',
+		scope: {
+			doubleClick : '&'
+		},
+		link: function(scope, element, attrs) {
+			element.on('dblclick', function(){
+				scope.doubleClick()
+			})
+		}
+	};
+});
